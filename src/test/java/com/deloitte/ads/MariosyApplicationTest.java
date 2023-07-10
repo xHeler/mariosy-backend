7719df9820f3f4924896a172f84be608062ec069
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-07-10 15:20:17 +0200

    chore: change files location

--- src/test/java/com/deloitte/ads/MariosyApplicationTest.java
+++ src/test/java/com/deloitte/ads/MariosyApplicationTest.java
@@ -1,38 +1,6 @@
 package com.deloitte.ads;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class MariosyApplicationTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public MariosyApplicationTest(String testName )
-    {
-        super( testName );
-    }
+public class MariosyApplicationTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( MariosyApplicationTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
