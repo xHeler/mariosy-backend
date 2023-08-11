2bcf203850cf3857c29671abb3d39858f1b092e0
Bartosz Kwiatkowski <kwiatkowsky.kontakt@gmail.com>
2023-08-11 10:40:39 +0200

    refactor: increese readibility of secure config class

--- src/main/java/com/deloitte/ads/configuration/SecurityConfig.java
+++ src/main/java/com/deloitte/ads/configuration/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,6 +27,7 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
     private final ApplicationContext context;
     private final JwtConverter jwtConverter;
     private Map<RequestMethod, List<String>> secureEndpointsMap;
@@ -43,228 +45,86 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private void setupSecurity(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
-
-        for (Map.Entry<RequestMethod, List<String>> entry : secureEndpointsMap.entrySet()) {
-            HttpMethod httpMethod = HttpMethod.resolve(entry.getKey().name());
-            for (String endpoint : entry.getValue()) {
-                http
-                        .authorizeRequests()
-                        .antMatchers(httpMethod, "/" + endpoint)
-                        .authenticated();
-            }
-        }
+        configureSecureEndpoints(http);
         http.authorizeRequests().anyRequest().permitAll();
     }
 
+    private void configureSecureEndpoints(HttpSecurity http) throws Exception {
+        secureEndpointsMap.forEach((requestMethod, endpoints) -> {
+            HttpMethod httpMethod = HttpMethod.resolve(requestMethod.name());
+            endpoints.forEach(endpoint -> {
+                try {
+                    http.authorizeRequests().antMatchers(httpMethod, "/" + endpoint).authenticated();
+                } catch (Exception e) {
+                    log.error("Error configuring endpoint: {}", endpoint, e);
+                }
+            });
+        });
+    }
+
     private void setupSession(HttpSecurity http) throws Exception {
-        http
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @PostConstruct
     public void init() {
         secureEndpointsMap = getSecureEndpointsMap();
-        log.info("Secure endpoints: " + secureEndpointsMap);
+        log.info("Secure endpoints: {}", secureEndpointsMap);
     }
 
     private Map<RequestMethod, List<String>> getSecureEndpointsMap() {
         Map<RequestMethod, List<String>> secureEndpointsMap = new HashMap<>();
-
-        Map<String, Object> beans = context.getBeansWithAnnotation(
-                RestController.class
-        );
-
-        for (Map.Entry<String, Object> entry : beans.entrySet()) {
-            Class<?> controllerClass = AopProxyUtils.ultimateTargetClass(
-                    entry.getValue()
-            );
-            String basePath = getBasePath(controllerClass);
-
-            secureEndpointsMap =
-                    handleControllerMethods(
-                            secureEndpointsMap,
-                            controllerClass,
-                            basePath
-                    );
-        }
-
+        context.getBeansWithAnnotation(RestController.class).values()
+                .forEach(bean -> updateSecureEndpointsMapForController(secureEndpointsMap, bean));
         return secureEndpointsMap;
+    }
+
+    private void updateSecureEndpointsMapForController(Map<RequestMethod, List<String>> map, Object bean) {
+        Class<?> controllerClass = AopProxyUtils.ultimateTargetClass(bean);
+        String basePath = getBasePath(controllerClass);
+        for (Method method : controllerClass.getDeclaredMethods()) {
+            if (method.isAnnotationPresent(SecureEndpoint.class)) {
+                handleAnnotationsForSecureEndpoints(map, method, basePath);
+            }
+        }
     }
 
     private String getBasePath(Class<?> controllerClass) {
-        RequestMapping controllerRequestMapping = AnnotatedElementUtils.findMergedAnnotation(
-                controllerClass,
-                RequestMapping.class
-        );
-        return (
-                controllerRequestMapping != null &&
-                        controllerRequestMapping.value().length > 0
-        )
-                ? controllerRequestMapping.value()[0]
-                : "";
+        RequestMapping mapping = AnnotatedElementUtils.findMergedAnnotation(controllerClass, RequestMapping.class);
+        return (mapping != null && mapping.value().length > 0) ? mapping.value()[0] : "";
     }
 
-    private Map<RequestMethod, List<String>> handleControllerMethods(
-            Map<RequestMethod, List<String>> secureEndpointsMap,
-            Class<?> controllerClass,
-            String basePath
-    ) {
-        Method[] methods = controllerClass.getDeclaredMethods();
-
-        for (Method method : methods) {
-            if (method.isAnnotationPresent(SecureEndpoint.class)) {
-                secureEndpointsMap =
-                        handleMethodAnnotations(
-                                secureEndpointsMap,
-                                method,
-                                basePath
-                        );
+    private void handleAnnotationsForSecureEndpoints(Map<RequestMethod, List<String>> map, Method method, String basePath) {
+        Class<?>[] annotations = {GetMapping.class, PostMapping.class, PutMapping.class, DeleteMapping.class, PatchMapping.class};
+        for (Class<?> annotationClass : annotations) {
+            Annotation annotation = AnnotatedElementUtils.findMergedAnnotation(method, (Class<Annotation>) annotationClass);
+            if (annotation != null) {
+                RequestMethod requestMethod = getRequestMethodForAnnotation(annotationClass);
+                updateEndpointMap(map, requestMethod, basePath, getPathsFromAnnotation(annotation));
             }
         }
-
-        return secureEndpointsMap;
     }
 
-    private Map<RequestMethod, List<String>> handleMethodAnnotations(
-            Map<RequestMethod, List<String>> secureEndpointsMap,
-            Method method,
-            String basePath
-    ) {
-        secureEndpointsMap =
-                handleGetMapping(secureEndpointsMap, method, basePath);
-        secureEndpointsMap =
-                handlePostMapping(secureEndpointsMap, method, basePath);
-        secureEndpointsMap =
-                handlePutMapping(secureEndpointsMap, method, basePath);
-        secureEndpointsMap =
-                handleDeleteMapping(secureEndpointsMap, method, basePath);
-        secureEndpointsMap =
-                handlePatchMapping(secureEndpointsMap, method, basePath);
-
-        return secureEndpointsMap;
+    private RequestMethod getRequestMethodForAnnotation(Class<?> annotationClass) {
+        if (annotationClass == GetMapping.class) return RequestMethod.GET;
+        if (annotationClass == PostMapping.class) return RequestMethod.POST;
+        if (annotationClass == PutMapping.class) return RequestMethod.PUT;
+        if (annotationClass == DeleteMapping.class) return RequestMethod.DELETE;
+        if (annotationClass == PatchMapping.class) return RequestMethod.PATCH;
+        throw new IllegalArgumentException("Unsupported annotation: " + annotationClass);
     }
 
-    private Map<RequestMethod, List<String>> handleGetMapping(
-            Map<RequestMethod, List<String>> secureEndpointsMap,
-            Method method,
-            String basePath
-    ) {
-        GetMapping getMapping = AnnotatedElementUtils.findMergedAnnotation(
-                method,
-                GetMapping.class
-        );
-        if (getMapping != null) {
-            secureEndpointsMap =
-                    updateEndpointMap(
-                            secureEndpointsMap,
-                            RequestMethod.GET,
-                            basePath,
-                            getMapping.value()
-                    );
-        }
-        return secureEndpointsMap;
+    private String[] getPathsFromAnnotation(Annotation annotation) {
+        if (annotation instanceof GetMapping) return ((GetMapping) annotation).value();
+        if (annotation instanceof PostMapping) return ((PostMapping) annotation).value();
+        if (annotation instanceof PutMapping) return ((PutMapping) annotation).value();
+        if (annotation instanceof DeleteMapping) return ((DeleteMapping) annotation).value();
+        if (annotation instanceof PatchMapping) return ((PatchMapping) annotation).value();
+        throw new IllegalArgumentException("Unsupported annotation: " + annotation);
     }
 
-    private Map<RequestMethod, List<String>> handlePostMapping(
-            Map<RequestMethod, List<String>> secureEndpointsMap,
-            Method method,
-            String basePath
-    ) {
-        PostMapping postMapping = AnnotatedElementUtils.findMergedAnnotation(
-                method,
-                PostMapping.class
-        );
-        if (postMapping != null) {
-            secureEndpointsMap =
-                    updateEndpointMap(
-                            secureEndpointsMap,
-                            RequestMethod.POST,
-                            basePath,
-                            postMapping.value()
-                    );
-        }
-        return secureEndpointsMap;
-    }
-
-    private Map<RequestMethod, List<String>> handlePutMapping(
-            Map<RequestMethod, List<String>> secureEndpointsMap,
-            Method method,
-            String basePath
-    ) {
-        PutMapping putMapping = AnnotatedElementUtils.findMergedAnnotation(
-                method,
-                PutMapping.class
-        );
-        if (putMapping != null) {
-            secureEndpointsMap =
-                    updateEndpointMap(
-                            secureEndpointsMap,
-                            RequestMethod.PUT,
-                            basePath,
-                            putMapping.value()
-                    );
-        }
-        return secureEndpointsMap;
-    }
-
-    private Map<RequestMethod, List<String>> handleDeleteMapping(
-            Map<RequestMethod, List<String>> secureEndpointsMap,
-            Method method,
-            String basePath
-    ) {
-        DeleteMapping deleteMapping = AnnotatedElementUtils.findMergedAnnotation(
-                method,
-                DeleteMapping.class
-        );
-        if (deleteMapping != null) {
-            secureEndpointsMap =
-                    updateEndpointMap(
-                            secureEndpointsMap,
-                            RequestMethod.DELETE,
-                            basePath,
-                            deleteMapping.value()
-                    );
-        }
-        return secureEndpointsMap;
-    }
-
-    private Map<RequestMethod, List<String>> handlePatchMapping(
-            Map<RequestMethod, List<String>> secureEndpointsMap,
-            Method method,
-            String basePath
-    ) {
-        PatchMapping patchMapping = AnnotatedElementUtils.findMergedAnnotation(
-                method,
-                PatchMapping.class
-        );
-        if (patchMapping != null) {
-            secureEndpointsMap =
-                    updateEndpointMap(
-                            secureEndpointsMap,
-                            RequestMethod.PATCH,
-                            basePath,
-                            patchMapping.value()
-                    );
-        }
-        return secureEndpointsMap;
-    }
-
-    private Map<RequestMethod, List<String>> updateEndpointMap(
-            Map<RequestMethod, List<String>> secureEndpointsMap,
-            RequestMethod requestMethod,
-            String basePath,
-            String[] paths
-    ) {
-        if (paths.length > 0) {
-            secureEndpointsMap
-                    .computeIfAbsent(requestMethod, k -> new ArrayList<>())
-                    .add(basePath + paths[0]);
-        } else {
-            secureEndpointsMap
-                    .computeIfAbsent(requestMethod, k -> new ArrayList<>())
-                    .add(basePath);
-        }
-        return secureEndpointsMap;
+    private void updateEndpointMap(Map<RequestMethod, List<String>> map, RequestMethod requestMethod, String basePath, String[] paths) {
+        String pathToUse = paths.length > 0 ? basePath + paths[0] : basePath;
+        map.computeIfAbsent(requestMethod, k -> new ArrayList<>()).add(pathToUse);
     }
 }
